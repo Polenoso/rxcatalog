99c87cd32b3b6984d59edbe75d309a8adc51fb6b
Aitor <aitor.p.n@gmail.com>
2018-11-25 11:08:41 +0100

    Project generated.
    	- Added modules
    	- Added pods
    	- Modified Gitignore
    	- Added entryPoint by plist (change to do it on AppDelegate)

---
+++ rxcatalog/rxcatalog/Scenes/Catalogs/CatalogsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CatalogsViewController.swift
+//  rxcatalog
+//
+//  Created by Aitor Pagán on 25/11/2018.
+//  Copyright © 2018 Aitor Pagán. All rights reserved.
+//
+
+import UIKit
+
+class CatalogsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
